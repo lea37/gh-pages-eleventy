f8cdf3419397cc608cfc5077d5ba47d3ee1be4a7
Lea Tortay <leatortay@gmail.com>
2020-07-04 08:44:42 +0200

    update cname

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      input: "./src/",
-      output: "./dist/",
-    },
-    templateFormats : ["njk", "md", "html"]
-  };
+    eleventyConfig.addPassthroughCopy('CNAME')
+    
+    return {
+        dir: {
+            input: "./src/",
+            output: "./dist/",
+        },
+        templateFormats : ["njk", "md", "html"]
+    };
 }
