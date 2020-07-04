ac0a553f77faf2c6a36dcb7f6fcb8e6531cf05d8
Lea Tortay <leatortay@gmail.com>
2020-07-04 08:45:51 +0200

    eleventy conf

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,8 @@ module.exports = function(eleventyConfig) {
     
     return {
         dir: {
-            input: "./src/",
-            output: "./dist/",
+            input: "src",
+            output: "dist",
         },
         templateFormats : ["njk", "md", "html"]
     };
